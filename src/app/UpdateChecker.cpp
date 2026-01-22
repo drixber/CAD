a63ee8071924d790361cb8b9aba1783d3db79bd3
Darius Herrmann <darius.herrmann@local>
2026-01-22 21:31:59 +0100

    Fix: Correct namespace in UpdateChecker.cpp

--- src/app/UpdateChecker.cpp
+++ src/app/UpdateChecker.cpp
@@ -9,7 +9,7 @@
 #include <QDir>
 
 namespace cad {
-namespace ui {
+namespace app {
 
 UpdateChecker::UpdateChecker(QObject* parent) : QObject(parent) {
     update_service_ = new cad::app::UpdateService();
@@ -108,6 +108,6 @@ void UpdateChecker::downloadUpdateInstaller(const QString& url) {
     });
 }
 
-}  // namespace ui
+}  // namespace app
 }  // namespace cad
 
